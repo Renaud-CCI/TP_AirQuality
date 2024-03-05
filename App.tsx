c809e6b733d90f5ea870b1a3668deeb68ddd60aa
Renaud Bresson <renaud.cci@gmail.com>
2024-03-05 13:48:06 +0100

    install/ReactNavigation-ok

--- App.tsx
+++ App.tsx
@@ -1,10 +1,27 @@
-import React from 'react';
-import {NativeBaseProvider, Box} from 'native-base';
+import * as React from 'react';
+import {NativeBaseProvider} from 'native-base';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import HomeContainer from './components/Home/HomeContainer';
+import DetailsContainer from './components/Details/DetailsContainer';
+
+const Stack = createNativeStackNavigator();
+
+function MyStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeContainer} />
+      <Stack.Screen name="Details" component={DetailsContainer} />
+    </Stack.Navigator>
+  );
+}
 
 export default function App() {
   return (
     <NativeBaseProvider>
-      <Box>Hello world</Box>
+      <NavigationContainer>
+        <MyStack />
+      </NavigationContainer>
     </NativeBaseProvider>
   );
 }
